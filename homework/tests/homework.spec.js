f644b8825275eff5642b58134d0430031044bdc5
Melanija <melanija.sibakovska1@gmail.com>
2022-07-24 17:20:59 +0300

    format

--- homework/tests/homework.spec.js
+++ homework/tests/homework.spec.js
@@ -267,29 +267,33 @@ data.forEach((version) => {
       await expect(page.locator("#numberAnswerField")).toHaveValue("10");
     });
 
-    test("Dividing 20.4 and 2.9 results in 7.0344827586206895", async ({ page }) => {
-        await page.goto("https://testsheepnz.github.io/BasicCalculator");
-        await page.selectOption("#selectBuild", { label: version });
-        await page.locator("#number1Field").type("20.4");
-        await page.locator("#number2Field").type("2.9");
-        await page.selectOption("#selectOperationDropdown", { label: "Divide" });
-        await page.locator("#calculateButton").click();
-  
-        await expect(page.locator("#numberAnswerField")).toHaveValue("7.0344827586206895");
-      });
+    test("Dividing 20.4 and 2.9 results in 7.0344827586206895", async ({
+      page,
+    }) => {
+      await page.goto("https://testsheepnz.github.io/BasicCalculator");
+      await page.selectOption("#selectBuild", { label: version });
+      await page.locator("#number1Field").type("20.4");
+      await page.locator("#number2Field").type("2.9");
+      await page.selectOption("#selectOperationDropdown", { label: "Divide" });
+      await page.locator("#calculateButton").click();
 
-      test("Dividing 20.4 and 2.9 results in 7", async ({ page }) => {
-        await page.goto("https://testsheepnz.github.io/BasicCalculator");
-        await page.selectOption("#selectBuild", { label: version });
-        await page.locator("#number1Field").type("20.4");
-        await page.locator("#number2Field").type("2.9");
-        await page.selectOption("#selectOperationDropdown", { label: "Divide" });
-        await page.selectOption("#integerSelect");
+      await expect(page.locator("#numberAnswerField")).toHaveValue(
+        "7.0344827586206895"
+      );
+    });
 
-        await page.locator("#calculateButton").click();
-  
-        await expect(page.locator("#numberAnswerField")).toHaveValue("7");
-      });
+    test("Dividing 20.4 and 2.9 results in 7", async ({ page }) => {
+      await page.goto("https://testsheepnz.github.io/BasicCalculator");
+      await page.selectOption("#selectBuild", { label: version });
+      await page.locator("#number1Field").type("20.4");
+      await page.locator("#number2Field").type("2.9");
+      await page.selectOption("#selectOperationDropdown", { label: "Divide" });
+      await page.selectOption("#integerSelect");
+
+      await page.locator("#calculateButton").click();
+
+      await expect(page.locator("#numberAnswerField")).toHaveValue("7");
+    });
   });
 });
 //DIVIDE STARTS
