bfc36747243afde4f0b12e1de679706359c0c200
Melanija <melanija.sibakovska1@gmail.com>
2022-07-24 18:03:13 +0300

    new

--- homework/tests/homework.spec.js
+++ homework/tests/homework.spec.js
@@ -5,19 +5,6 @@ const data = ["Prototype", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 //ADD STARTS
 data.forEach((version) => {
   test.describe(version + ": Add", () => {
-    test("Adding 100 and 350 results in 450", async ({ page }) => {
-      await page.goto("https://testsheepnz.github.io/BasicCalculator");
-      await page.selectOption("#selectBuild", { label: version });
-      await page.locator("#number1Field").type("100");
-      await page.locator("#number2Field").type("350");
-      await page.selectOption("#selectOperationDropdown", {
-        label: "Add",
-      });
-      await page.locator("#calculateButton").click();
-
-      await expect(page.locator("#numberAnswerField")).toHaveValue("450");
-    });
-
     test("Adding -10 and 13 results in 3", async ({ page }) => {
       await page.goto("https://testsheepnz.github.io/BasicCalculator");
       await page.selectOption("#selectBuild", { label: version });
@@ -30,7 +17,11 @@ data.forEach((version) => {
 
       await expect(page.locator("#numberAnswerField")).toHaveValue("3");
     });
+  });
+});
 
+data.forEach((version) => {
+  test.describe(version + ": Add", () => {
     test("Adding 2.5 and 6.7 results in 9.2", async ({ page }) => {
       await page.goto("https://testsheepnz.github.io/BasicCalculator");
       await page.selectOption("#selectBuild", { label: version });
@@ -43,7 +34,11 @@ data.forEach((version) => {
 
       await expect(page.locator("#numberAnswerField")).toHaveValue("9.2");
     });
+  });
+});
 
+data.forEach((version) => {
+  test.describe(version + ": Add", () => {
     test("Adding 0 and -4 results in -4", async ({ page }) => {
       await page.goto("https://testsheepnz.github.io/BasicCalculator");
       await page.selectOption("#selectBuild", { label: version });
@@ -56,7 +51,11 @@ data.forEach((version) => {
 
       await expect(page.locator("#numberAnswerField")).toHaveValue("-4");
     });
+  });
+});
 
+data.forEach((version) => {
+  test.describe(version + ": Add", () => {
     test("Adding -6 and 0 results in -6", async ({ page }) => {
       await page.goto("https://testsheepnz.github.io/BasicCalculator");
       await page.selectOption("#selectBuild", { label: version });
@@ -69,7 +68,11 @@ data.forEach((version) => {
 
       await expect(page.locator("#numberAnswerField")).toHaveValue("-6");
     });
+  });
+});
 
+data.forEach((version) => {
+  test.describe(version + ": Add", () => {
     test("Adding 100 and 350 results in 450", async ({ page }) => {
       await page.goto("https://testsheepnz.github.io/BasicCalculator");
       await page.selectOption("#selectBuild", { label: version });
@@ -82,7 +85,11 @@ data.forEach((version) => {
 
       await expect(page.locator("#numberAnswerField")).toHaveValue("450");
     });
+  });
+});
 
+data.forEach((version) => {
+  test.describe(version + ": Add", () => {
     test("Adding 45.7 and 3 results in 48", async ({ page }) => {
       await page.goto("https://testsheepnz.github.io/BasicCalculator");
       await page.selectOption("#selectBuild", { label: version });
